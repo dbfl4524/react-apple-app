ffe9f2aaafe1ecc0cb5dfa56390f9c9a72e40052
dbfl4524 <sinyuri0525@gmail.com>
2024-02-27 15:08:16 +0900

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 
 const Nav = () => {
   const [show, setShow] = useState("false");
+
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   const listener = () => {
     if (window.scrollY > 50) {
@@ -20,6 +24,11 @@ const Nav = () => {
     };
   }, []);
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <NavWrapper show={show}>
       <Logo>
@@ -29,9 +38,46 @@ const Nav = () => {
           onClick={() => (window.location.href = "/")}
         />
       </Logo>
+
+      <input
+        type="text"
+        placeholder="영화를 검색해주세요"
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+      />
+
+      <Login>로그인</Login>
     </NavWrapper>
   );
 };
+
+const input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 5px;
+  color: white;
+  padding: 5px;
+  border: 1px solid lightgray;
+`;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
 
 const Logo = styled.a`
   padding: 0;
